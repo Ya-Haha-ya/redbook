488e299bae766158af3e5c48b74ad85803a7a5f4
Xia Zhao <xiazhao@xias-mac-studio.lan>
2022-09-07 23:34:21 -0700

    [swagger][springdoc] Integrate with springdoc to provide documentation for controller layer.

--- src/main/kotlin/com/xiazhao/redbook/controller/PostController.kt
+++ src/main/kotlin/com/xiazhao/redbook/controller/PostController.kt
@@ -2,6 +2,9 @@ package com.xiazhao.redbook.controller
 
 import com.xiazhao.redbook.payload.PostDto
 import com.xiazhao.redbook.service.PostService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -16,7 +19,14 @@ class PostController(private val postService: PostService) {
     fun getAllPosts() = ResponseEntity.ok(postService.getAllPosts())
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun createPost(@Valid @RequestBody postDto: PostDto) = ResponseEntity(postService.createPost(postDto), HttpStatus.CREATED)
+    @Operation(summary = "Create a new post with PostDto", description = "Creates a new post with PostDto.")
+    @Parameter(name = "postDto", description = "Post details with title, description and content.")
+    @ApiResponse(responseCode = "201", description = "New post created successful")
+    fun createPost(
+        @Valid
+        @RequestBody
+        postDto: PostDto
+    ) = ResponseEntity(postService.createPost(postDto), HttpStatus.CREATED)
 
     @GetMapping("/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getPostById(@PathVariable(name = "id") id: Long) = ResponseEntity.ok(postService.getPostById(id))
