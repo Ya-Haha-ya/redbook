9a3dab1495566d4ec97e49db38eb89fe1bf5fb9d
Xia Zhao <xiazhao@xias-mac-studio.lan>
2022-09-11 00:42:36 -0700

    [8/n][feature/post] Add log for service apis.

--- src/main/kotlin/com/xiazhao/redbook/service/impl/PostServiceImpl.kt
+++ src/main/kotlin/com/xiazhao/redbook/service/impl/PostServiceImpl.kt
@@ -6,6 +6,7 @@ import com.xiazhao.redbook.exception.ResourceNotFoundException
 import com.xiazhao.redbook.payload.PostDto
 import com.xiazhao.redbook.service.PostService
 import org.modelmapper.ModelMapper
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,13 +15,20 @@ class PostServiceImpl(
     private val modelMapper: ModelMapper,
 ) : PostService {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(PostServiceImpl::class.java)
+    }
+
     override fun getAllPosts(): List<PostDto> = postRepository.findAll().map { it.mapToDto() }
+        .also { logger.info("getAllPost() get called") }
 
     override fun createPost(postDto: PostDto): PostDto =
         postRepository.save(modelMapper.map(postDto, Post::class.java)).mapToDto()
+            .also { logger.info("createPost() get called") }
 
     override fun getPostById(id: Long): PostDto =
         postRepository.findById(id).orElseThrow { postNotFoundException(id) }.mapToDto()
+            .also { logger.info("getPostById() get called") }
 
     override fun updatePostById(id: Long, postDto: PostDto): PostDto =
         with(postRepository.findById(id).orElseThrow { postNotFoundException(id) }) {
@@ -28,10 +36,11 @@ class PostServiceImpl(
             description = postDto.description
             content = postDto.content
             postRepository.save(this).mapToDto()
-        }
+        }.also { logger.info("updatePostById() get called") }
 
     override fun deletePostById(id: Long) =
         postRepository.findById(id).orElseThrow { postNotFoundException(id) }.let { postRepository.delete(it) }
+            .also { logger.info("deletePostById() get called") }
 
     private fun postNotFoundException(id: Long) = ResourceNotFoundException("Post", "id", id)
 
